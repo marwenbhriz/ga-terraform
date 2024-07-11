6b91e3e849020750b27f4c928f18d5858f43153b
benhriz <benhriz@grasys.io>
2024-07-11 20:01:07 +0900

    fix cluster ressource name

--- modules/gke/outputs.tf
+++ modules/gke/outputs.tf
@@ -7,10 +7,10 @@ output "token" {
 }
 
 output "host" {
-  value = google_container_cluster.benhriz-cluster.endpoint
+  value = google_container_cluster.ga-cluster.endpoint
 }
 
 output "cluster_ca_certificate" {
-  value = google_container_cluster.benhriz-cluster.master_auth[0].cluster_ca_certificate
+  value = google_container_cluster.ga-cluster.master_auth[0].cluster_ca_certificate
 }
 
