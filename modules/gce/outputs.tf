bccfc9e7db3f6d63d0fff438eb306bb78da744cd
benhriz <benhriz@grasys.io>
2024-07-11 19:42:50 +0900

    fix no output for nat ip for gce module

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -1,7 +1,3 @@
 output "instances" {
   value = google_compute_instance.default.*.self_link
 }
-
-output "nat_ip" {
-  value = google_compute_instance.default[*].network_interface[0].access_config[0].nat_ip
-}
