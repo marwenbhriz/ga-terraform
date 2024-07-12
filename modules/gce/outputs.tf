1d772d83a5d31a1720d0096566fa8796f4f43ddf
benhriz <benhriz@grasys.io>
2024-07-12 16:12:17 +0900

    fix jump ops ip output

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -3,6 +3,6 @@ output "instance" {
 }
 
 output "ip" {
-  value       = google_compute_instance.default.network_interface.0.network_ip
+  value = google_compute_instance.default[*].network_interface[0].network_ip
   description = "The IP address of the jump ops instance."
 }
