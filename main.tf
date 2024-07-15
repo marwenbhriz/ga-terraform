76952ea720115788e9bd93138225feaad946eef2
Marwen Ben Hriz <142993710+marwenbhriz@users.noreply.github.com>
2024-07-15 16:05:37 +0900

    add output

---
+++ main.tf
@@ -0,0 +1,72 @@
+// import pubsub module
+module "pubsub" {
+  source       = "./modules/pubsub"
+}
+
+// import network module
+module "network" {
+  source         = "./modules/network"
+  project_name   = var.accounts.project
+  project_region = var.accounts.region
+}
+
+// import gcr module
+module "gcr" {
+  source         = "./modules/gcr"
+  project_name   = var.accounts.project
+  project_region = var.accounts.region
+}
+
+// import jump ops firewall module
+module "firewall" {
+  source       = "./modules/firewall"
+  project_name = var.accounts.project
+  network_name = module.network.network_name
+
+  // need to wait until network modules creation
+  depends_on   = [ module.network ]
+}
+
+
+// import gce module
+module "gce" {
+  source        = "./modules/gce"
+  project_name  = var.accounts.project
+  network       = module.network.network
+  firewall_name = module.firewall.firewall_name
+  subnetwork    = module.network.subnetwork
+  project_zone  = var.accounts.zone
+
+  // need to wait until firewall and network modules creation
+  depends_on    = [ module.network ]
+}
+
+// import gke module
+module "gke" {
+  source                     = "./modules/gke"
+  project_name               = var.accounts.project
+  project_region             = var.accounts.region
+  network                    = module.network.network
+  subnetwork                 = module.network.subnetwork
+  topic_id                   = module.pubsub.topic_id
+  #ip_cidr_range = module.network.ip_cidr_range
+  authorized_ipv4_cidr_block = "${module.gce.ip[0]}/32"
+
+  depends_on                 = [ module.pubsub, module.network, module.gce ]
+}
+
+// import spanner module
+module "cloudsql" {
+  source            = "./modules/cloudsql"
+  region            = var.accounts.region
+  network           = module.network.network
+  network_self_link = module.network.self_link
+  network_id        = module.network.id
+
+  depends_on        = [ module.network ]
+}
+
+// import spanner module
+//module "spanner" {
+//  source       = "./modules/spanner"
+//}
---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "database_ip" {
+  value = module.cloudsql.public_ip_address
+}
+
+output "database_password" {
+  value = module.cloudsql.password
+}
+
+output "database_name" {
+  value = module.cloudsql.database_name
+}
